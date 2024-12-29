deb5f59ce41888e887d5dece79826b3c8e850b38
abhsihek12 <abhishek8070@gmail.com>
2024-12-29 15:00:14 +0530

    my first commit

---
+++ testingBabaAutomation/src/main/java/baselibrary/Baselibrary.java
@@ -0,0 +1,18 @@
+package baselibrary;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import io.github.bonigarcia.wdm.webdriver.WebDriverBrowser;
+
+public class Baselibrary {
+
+    @Test
+	public void launchurl()
+	{
+		//WebDriverManager.chromedriver().setup();
+		 WebDriver driver=new ChromeDriver();
+	}
+}
